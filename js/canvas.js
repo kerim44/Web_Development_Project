61770faf73e0dd8707ba91b1062f9f78d423b51a
Kerim <kreimyilmaz@gmail.com>
2022-10-28 23:46:28 +0300

    28-10 change color add login screen

---
+++ js/canvas.js
@@ -0,0 +1,10 @@
+window.addEventListener("load", () => {
+  const canvas = document.querySelector("#canvas");
+  const ctx = canvas.getContext("2d");
+
+  console.log("asdsad");
+
+  //Boyut
+  canvas.height = window.innerHeight;
+  canvas.width = window.innerWidth;
+});
