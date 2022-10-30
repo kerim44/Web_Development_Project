752fc1a93e3493f040a50016afd5f959037f5e60
Kerim Yılmaz <kreimyilmaz@gmail.com>
2022-10-30 23:48:26 +0300

    optimize canvas and buttonbar
    renk ve kalınlık değişti, çizginin yeri window'a bağlandı. buttonbarın yeri düzeltildi.

--- js/canvas.js
+++ js/canvas.js
@@ -1,20 +1,21 @@
 window.addEventListener("load", () => {
+  var a = window.innerHeight * 0.118;
+  var b = window.innerWidth * 0.0055
+
   const canvas = document.querySelector("#canvas");
   const ctx = canvas.getContext("2d");
 
   console.log("asdsad");
 
   canvas.height = window.innerHeight;
-  canvas.width = window.innerWidth;
+  canvas.width = 1770;
 
 
   let painting = false;
-  ctx.fillStyle ='white'
-
 
 
   function startPosition(e){
-    ctx.moveTo(e.clientX,e.clientY-108);
+    ctx.moveTo(e.clientX-b,e.clientY-a);
     painting = true;
   }
 
@@ -24,10 +25,12 @@ window.addEventListener("load", () => {
 
   function draw(e){
     if(!painting) return;
-    ctx.lineWidth = 10;
+    ctx.lineWidth = 2;
     ctx.lineCap = "round";
-    ctx.lineTo(e.clientX,e.clientY-108);
+    ctx.lineTo(e.clientX-b,e.clientY-a);
     ctx.stroke();
+    ctx.strokeStyle ='green'
+    
   }
 
   
