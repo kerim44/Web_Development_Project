029f0b3fbcab51d7b5192907ccdd2581dd996452
Bahadir-Atilgan <105520682+Bahadir-Atilgan@users.noreply.github.com>
2022-10-30 18:51:01 +0300

    canvas degisikligi
    canva.js dosyasındaki fonksiyonlar primitif olarak oluşturuldu

--- js/canvas.js
+++ js/canvas.js
@@ -4,7 +4,32 @@ window.addEventListener("load", () => {
 
   console.log("asdsad");
 
-  //Boyut
   canvas.height = window.innerHeight;
   canvas.width = window.innerWidth;
+
+
+  let painting = false;
+
+  function startPosition(e){
+    ctx.moveTo(e.clientX,e.clientY);
+    painting = true;
+  }
+
+  function finishedPosition(){
+    painting = false;
+  }
+
+  function draw(e){
+    if(!painting) return;
+    ctx.lineWidth = 10;
+    ctx.lineCap = "round";
+    ctx.lineTo(e.clientX,e.clientY);
+    ctx.stroke();
+  }
+
+  
+
+  canvas.addEventListener("mousedown", startPosition)
+  canvas.addEventListener("mouseup", finishedPosition)
+  canvas.addEventListener("mousemove", draw)
 });
