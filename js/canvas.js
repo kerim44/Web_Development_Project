402f0182c096b44db3302262a4874c4be544cc48
Kerim <kreimyilmaz@gmail.com>
2022-10-31 18:07:42 +0300

    dosya görünürlüğü, önemli değil

--- js/canvas.js
+++ js/canvas.js
@@ -1,6 +1,6 @@
 window.addEventListener("load", () => {
   var a = window.innerHeight * 0.118;
-  var b = window.innerWidth * 0.0055
+  var b = window.innerWidth * 0.0055;
 
   const canvas = document.querySelector("#canvas");
   const ctx = canvas.getContext("2d");
@@ -10,32 +10,27 @@ window.addEventListener("load", () => {
   canvas.height = window.innerHeight;
   canvas.width = 1770;
 
-
   let painting = false;
 
-
-  function startPosition(e){
-    ctx.moveTo(e.clientX-b,e.clientY-a);
+  function startPosition(e) {
+    ctx.moveTo(e.clientX - b, e.clientY - a);
     painting = true;
   }
 
-  function finishedPosition(){
+  function finishedPosition() {
     painting = false;
   }
 
-  function draw(e){
-    if(!painting) return;
+  function draw(e) {
+    if (!painting) return;
     ctx.lineWidth = 2;
     ctx.lineCap = "round";
-    ctx.lineTo(e.clientX-b,e.clientY-a);
+    ctx.lineTo(e.clientX - b, e.clientY - a);
     ctx.stroke();
-    ctx.strokeStyle ='green'
-    
+    ctx.strokeStyle = "green";
   }
 
-  
-
-  canvas.addEventListener("mousedown", startPosition)
-  canvas.addEventListener("mouseup", finishedPosition)
-  canvas.addEventListener("mousemove", draw)
+  canvas.addEventListener("mousedown", startPosition);
+  canvas.addEventListener("mouseup", finishedPosition);
+  canvas.addEventListener("mousemove", draw);
 });
