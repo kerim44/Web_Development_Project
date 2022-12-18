97189fe9aa7c6e270efb92b63dc38cfe1f553ce9
Bahadir-Atilgan <105520682+Bahadir-Atilgan@users.noreply.github.com>
2022-12-19 00:31:00 +0300

    firebase 2
    firebsae firepad init funtions

--- public/firebase.js
+++ public/firebase.js
@@ -1,17 +1,15 @@
-// Import the functions you need from the SDKs you need
+
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyBOuCWrezIyym-41Sj2TDylNqNEX_QNvZo",
-  authDomain: "webdev-c90c5.firebaseapp.com",
-  projectId: "webdev-c90c5",
-  storageBucket: "webdev-c90c5.appspot.com",
-  messagingSenderId: "38334752607",
-  appId: "1:38334752607:web:886d59914780b1cd4db8ea"
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
+function init() {
+  // Initialize Firebase.
+  var config = {
+    apiKey: "AIzaSyBOuCWrezIyym-41Sj2TDylNqNEX_QNvZo",
+    authDomain: "webdev-c90c5.firebaseapp.com",
+    projectId: "webdev-c90c5",
+    storageBucket: "webdev-c90c5.appspot.com",
+    messagingSenderId: "38334752607",
+    appId: "1:38334752607:web:886d59914780b1cd4db8ea"
+  };
+  firebase.initializeApp(config);
+}
