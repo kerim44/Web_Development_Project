82c91fc52d886e36226151ee79f9092a845b043e
Bahadir-Atilgan <105520682+Bahadir-Atilgan@users.noreply.github.com>
2023-01-02 01:51:06 +0300

    code satırı düzeltildi

--- public/code.js
+++ public/code.js
@@ -1,22 +1,16 @@
 window.addEventListener("load", () => {
-//const socket =  io.connect('http://localhost:3000')
 const button3 = document.getElementById('button3')
 const editor = CodeMirror.fromTextArea(document.getElementById("CodeWindow"), {
   lineNumbers: true,
   mode: 'javascript'
 });
-editor.getDoc().setValue('    CODE HERE!');
+editor.getDoc().setValue("\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
 
 button3.addEventListener('click',() => {
   console.log(editor.getValue());
   socket.emit('codeSend',editor.getValue());
 })
-
 socket.on('codeRecive',code=>{
   editor.getDoc().setValue(code);
 })
-
 })
-
-
-
