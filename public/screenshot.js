e0afdeb10309cad780758e3b8f63a78e61532feb
Bahadir-Atilgan <105520682+Bahadir-Atilgan@users.noreply.github.com>
2023-01-02 01:13:30 +0300

    screenshot
    screenshot function is completed

--- public/screenshot.js
+++ public/screenshot.js
@@ -1,13 +1,12 @@
 window.addEventListener("load", () => {
     console.log("we are in")
-    const button = document.getElementById('clear')
-    const canvas = document.querySelector("#canvas");
-    const ctx = canvas.getContext("2d");
-    var dataURL = canvas.toDataURL();
-
-    button.addEventListener('click',() => {
-        console.log("save?");
+    const button5 = document.getElementById('save');
+    button5.addEventListener('click',() => {
+        console.log("screenshot baby!");
+        
+        const canvas = document.getElementById('canvas');
+        const img    = canvas.toDataURL('image/png');
+        var win = window.open();
+        win.document.write('<iframe src="' + img + '" frameborder="0" style="border:0; top:0px; left:0px; bottom:0px; right:0px; width:100%; height:100%;" allowfullscreen></iframe>')
     })
-
-   
 })
